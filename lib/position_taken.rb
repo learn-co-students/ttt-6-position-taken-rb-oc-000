31af8402b5b3dbed8c9f34e75a3b5ab3d3c42be7
kraigslist <kraig.tapang@gmail.com>
2016-02-22 06:47:45 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,21 @@
-# code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, position, player="X")
+  board[position.to_i-1]=player
+end
+
+def position_taken?(board, position)
+			  if board[position.to_i] == "" || board[position.to_i] == " " || board[position.to_i] == nil
+			    false
+			  else
+			    true
+end
+end
